61ff0562409385912add90c81f0c13142db9aac8
Dmitry <d@kozlov.ru>
2026-06-09 22:25:29 +0700

    FileInfo: Added a separate full path

--- Sources/HubUI/Files.swift
+++ Sources/HubUI/Files.swift
@@ -107,7 +107,7 @@ public struct HubFiles: View {
     
     private var directories: [FileInfo] {
       uploadManager.directories(for: hub, at: path, with: list.directories, context: context)
-        .map { FileInfo(name: $0, size: 0, lastModified: nil) }
+        .map { FileInfo(path: path + $0, size: 0, lastModified: nil) }
         .sorted(using: sortOrder)
     }
     private var files: [FileInfo] {
@@ -259,14 +259,27 @@ struct FileList: Decodable {
   var directories: [String]
 }
 struct FileInfo: Identifiable, Hashable, Decodable {
-  var id: String { name }
   let name: String
-  var isDirectory: Bool { name.last == "/" }
+  let path: String
+  let size: Int
+  let lastModified: Date?
+  
+  var id: String { name }
+  let isDirectory: Bool
   var ext: String {
     isDirectory ? "" : String(name.split { $0 == "." }.last!)
   }
-  let size: Int
-  let lastModified: Date?
+  init(path: String, size: Int, lastModified: Date?) {
+    if let last = path.split(separator: "/").last(where: { !$0.isEmpty }) {
+      self.name = String(last)
+    } else {
+      self.name = path
+    }
+    self.path = path
+    self.size = size
+    self.lastModified = lastModified
+    self.isDirectory = path.last == "/"
+  }
 }
 
 // MARK: Extensions
